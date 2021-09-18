4b0663a93d29c6c2f11642f8c93ac45fbe7265aa
maciej790 <maciejmw126@gmail.com>
2021-09-18 18:46:51 +0200

    base setup styled-components in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home.js";
 import { Details } from "./pages/Details/Details.js";
+import { GlobalStyle } from "./style/GlobalStyles.js";
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Switch>
         <Route exact path="/">
           <Home />
---
+++ src/style/Variables.js
@@ -0,0 +1,19 @@
+export const colors = {
+  DarkBlue: "hsl(209, 23%, 22%)", //Dark Mode Elements
+  VeryDarkBlue1: "hsl(207, 26%, 17%)", //Dark Mode Background
+  VeryDarkBlue2: "hsl(207, 26%, 17%)", //Light Mode Text
+  DarkGray: "hsl(0, 0%, 52%)", //Light Mode Input
+  VeryLightGray: "hsl(0, 0%, 98%)", //Light Mode Background
+  White: "hsl(0, 0%, 100%)", //Dark Mode Text & Light Mode Elements
+};
+
+export const rwd = {
+  Mobile: "375px",
+  Desktop: "1440px",
+};
+
+export const font = {
+  Family: "'Nunito Sans', sans-serif",
+  HomeItems: "14px",
+  DetailPage: "16px",
+};
