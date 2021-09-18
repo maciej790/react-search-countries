5490ca2b99e28a597661d327afb9d59e834bbeb2
maciej790 <maciejmw126@gmail.com>
2021-09-18 17:01:08 +0200

    base setup routing in app

---
+++ src/pages/Details/Details.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useParams } from "react-router";
+
+export function Details() {
+  let { id } = useParams();
+  return (
+    <div>
+      <h1>Details {id}</h1>
+    </div>
+  );
+}
