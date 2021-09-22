ea4abdd1532b701d57c63363a236f23db43d5b6c
maciej790 <maciejmw126@gmail.com>
2021-09-22 18:56:05 +0200

    fixed warning in CountryItem

--- src/components/ResultItems/CountryItem/CountryItem.js
+++ src/components/ResultItems/CountryItem/CountryItem.js
@@ -6,9 +6,7 @@ export const CountryItem = () => {
     <Item>
       <Image />
       <Description>
-        <Title>
-          <h3>Germany</h3>
-        </Title>
+        <Title>Germany</Title>
         <p>Population: 81,770,900</p>
         <p>Region: Europe</p>
         <p>Capital: Berlin</p>
